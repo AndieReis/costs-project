3fbe74a41e09044ac2f88ae013037d4b886f53f0
Andie Reis <andie.reis@gmail.com>
2024-03-27 09:55:47 -0300

    Add Home

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/pages/Home';
 import Projects from './components/pages/Projects';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
 
 import Container from './components/layout/Container';
 import Footer from './components/layout/Footer';
@@ -22,6 +23,7 @@ function App() {
           <Route path='projects' element={<Projects />} />
           <Route path='company' element={<Company />} />
           <Route path='contact' element={<Contact />} />
+          <Route path='newproject' element={<NewProject />} />
         </Routes>
       </Container>
 
